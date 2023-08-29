aabc7072b569b6450335ea83c53721fa8c55b3b3
Alexff <matveevalexander79@gmail.com>
2023-08-29 15:51:36 +0300

    TIC001: add func plus

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('index.html')
 
+def plus(a, b):
+    return a+b
 
 if __name__ == '__main__':
     app.run()
+    print(plus(3, 4))
